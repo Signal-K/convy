e47d66628a2562600890744ce27e981aae3f160a
Gizmotronn <liam@skinetics.tech>
2025-06-14 17:20:06 -0500

    💍🌓 ↝ Initial supabase package setup

---
+++ Convo/ConvoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvoApp.swift
+//  Convo
+//
+//  Created by Liam Arbuckle on 14/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct ConvoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
